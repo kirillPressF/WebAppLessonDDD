8fafd7f6bc29655b53acb0245316e2a881012cb5
kirillPressF <prohorenko25@gmail.com>
2024-03-09 01:33:52 +0200

    Добавьте файлы проекта.

---
+++ WebAppLessonDDD.Core/Models/News.cs
@@ -0,0 +1,16 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace WebAppLessonDDD.Core.Models;
+
+public class News
+{
+    public class News
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string TextData { get; set; } = string.Empty;
+        public DateTime CreateDate { get; }
+        public int Views { get; } = 0;
+        public Image? TitleImage { get; }
+    }
+}
